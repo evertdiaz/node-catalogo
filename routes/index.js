48e2601f45de4317a42fb7bc9c6e69513ee47352
Evert Diaz <evertdiazb@gmail.com>
2016-11-19 19:46:18 -0500

    Terminada la subida, terminada la acoplacion de las vistas, solo falta vista de producto unitaria y de promociones

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var Promocion = require('../models').promocion
+var Producto = require('../models').producto
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Joyería Quisuruco' });
+  Producto.find({}, function(err, productos) {
+    res.render('index', { data: productos, title: 'Joyería Quisuruco' });   
+  })
 });
 
 router.get('/quienes-somos', function(req, res, next) {
@@ -16,8 +19,8 @@ router.get('/contactanos', function(req, res, next) {
 });
 
 router.get('/promociones', function(req, res, next) {
-	Promocion.find(function(err,promos) {
-		res.render('promociones', { data: promos, title: 'Promociones' });
-	})
+  Promocion.find(function(err,promos) {
+    res.render('promociones', { data: promos, title: 'Promociones' });
+  })
 });
 module.exports = router;
